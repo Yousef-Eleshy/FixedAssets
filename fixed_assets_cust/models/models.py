c814effdc4429c05d3e0868e8ee8ffaaca7bb01d
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-06-11 12:33:13 +0000

    Added Journal Entry Function

--- fixed_assets_cust/models/models.py
+++ fixed_assets_cust/models/models.py
@@ -1,40 +1,55 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from dateutil.relativedelta import relativedelta
+from odoo.exceptions import ValidationError, UserError
 
 
-class fixed_assets_cust(models.Model):
-    
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    from_custom_asset = fields.Boolean(string='From Custom Asset', copy=False)
+    asset_ref_id = fields.Many2one('account.asset', string='From Custom Asset', copy=False)
+
+
+class AccountAsset(models.Model):
     _inherit = 'account.asset'
-    
-    acc_nineteen = fields.Float(string='Acc - 2019', readonly=True)
-    
-    #acc_nineteen = fields.Monetary(string='Acc - 2019', readonly=True)
-    
-    #original_value = fields.Monetary(string="Original Value", compute='_compute_value', inverse='_set_value', store=True, readonly=True, states={'draft': [('readonly', False)]})
-    
-#     @api.depends('original_move_line_ids', 'original_move_line_ids.account_id', 'asset_type')
-#    def _compute_value(self):
-#        for record in self:
-#            misc_journal_id = self.env['account.journal'].search([('type', '=', 'general'), ('company_id', '=', record.company_id.id)], limit=1)
-#            if not record.original_move_line_ids:
-#                record.account_asset_id = record.account_asset_id or False
-#                record.original_value = record.original_value or False
-#                record.display_model_choice = record.state == 'draft' and self.env['account.asset'].search([('state', '=', 'model'), ('asset_type', '=', record.asset_type)])
-#                record.display_account_asset_id = True
-#                continue
-#            if any(line.move_id.state == 'draft' for line in record.original_move_line_ids):
-#                raise UserError(_("All the lines should be posted"))
-#           if any(account != record.original_move_line_ids[0].account_id for account in record.original_move_line_ids.mapped('account_id')):
-#                raise UserError(_("All the lines should be from the same account"))
-#            record.account_asset_id = record.original_move_line_ids[0].account_id
-#            record.display_model_choice = record.state == 'draft' and len(self.env['account.asset'].search([('state', '=', 'model'), ('account_asset_id.user_type_id', '=', record.user_type_id.id)]))
-#            record.display_account_asset_id = False
-#            if not record.journal_id:
-#                record.journal_id = misc_journal_id
-#            total_credit = sum(line.credit for line in record.original_move_line_ids)
-#            total_debit = sum(line.debit for line in record.original_move_line_ids)
-#            record.original_value = total_credit + total_debit
-#            if (total_credit and total_debit) or record.original_value == 0:
-#                raise UserError(_("You cannot create an asset from lines containing credit and debit on the account or with a null amount"))
 
+    acc_nineteen = fields.Float(string='Acc - 2019', readonly=False)
+    hide_custom_button = fields.Boolean(string='Hide GL - 2019 Button', default=False, copy=False)
+
+    def open_asset_custom_entries(self):
+        return {
+            'name': _('Journal Entries'),
+            'view_mode': 'tree,form',
+            'res_model': 'account.move',
+            'views': [(self.env.ref('account.view_move_tree').id, 'tree'), (False, 'form')],
+            'type': 'ir.actions.act_window',
+            'domain': [('from_custom_asset', '=', True), ('asset_ref_id', '=', self.id)],
+            'context': dict(self._context, create=False),
+        }
+
+    def create_custom_asset_entry(self):
+        for asset in self:
+            asset.compute_custom_depreciation_board()
+            asset.write({'hide_custom_button': True})
+
+    def compute_custom_depreciation_board(self):
+        self.ensure_one()
+        depreciation_number = self.method_number
+        if self.prorata:
+            depreciation_number += 1
+        starting_sequence = 0
+        amount_to_depreciate = self.acc_nineteen
+        depreciation_date = self.first_depreciation_date
+        commands = []
+        newlines = self._recompute_board(depreciation_number, starting_sequence, amount_to_depreciate,
+                                         depreciation_date, already_depreciated_amount=0, amount_change_ids=False)
+        newline_vals_list = []
+        for newline_vals in newlines:
+            # no need of amount field, as it is computed and we don't want to trigger its inverse function
+            del (newline_vals['amount_total'])
+            newline_vals_list.append(newline_vals)
+        new_moves = self.env['account.move'].create(newline_vals_list)
+        for move in new_moves:
+            move.write({'asset_ref_id': self.id, 'asset_id': False, 'from_custom_asset': True})

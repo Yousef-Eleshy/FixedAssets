8c6257e3cf90724e2e97bcf8f432887798313b78
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-06-18 14:33:03 +0000

    Removed GL Button and Added Acc Date Selection

--- fixed_assets_cust/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ fixed_assets_cust/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -20,13 +20,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'account_accountant', 'account_asset'],
+    'depends': ['base', 'account', 'account_accountant', 'account_asset','mrp'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/settings_view.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- fixed_assets_cust/models/models.py
+++ fixed_assets_cust/models/models.py
@@ -4,6 +4,7 @@ from odoo import models, fields, api, _
 from dateutil.relativedelta import relativedelta
 from odoo.exceptions import ValidationError, UserError
 from odoo.tools import format_date
+from datetime import datetime
 
 MAX_NAME_LENGTH = 50
 
@@ -19,30 +20,32 @@ class AccountAsset(models.Model):
     _inherit = 'account.asset'
 
     acc_nineteen = fields.Float(string='Acc - 2019', readonly=False)
-    hide_custom_button = fields.Boolean(string='Hide GL - 2019 Button', default=False, copy=False)
+#    hide_custom_button = fields.Boolean(string='Hide GL - 2019 Button', default=False, copy=False)
 
-    def open_asset_custom_entries(self):
-        return {
-            'name': _('Journal Entries'),
-            'view_mode': 'tree,form',
-            'res_model': 'account.move',
-            'views': [(self.env.ref('account.view_move_tree').id, 'tree'), (False, 'form')],
-            'type': 'ir.actions.act_window',
-            'domain': [('from_custom_asset', '=', True), ('asset_ref_id', '=', self.id)],
-            'context': dict(self._context, create=False),
-        }
+#    def open_asset_custom_entries(self):
+#        return {
+#            'name': _('Journal Entries'),
+#            'view_mode': 'tree,form',
+#            'res_model': 'account.move',
+#            'views': [(self.env.ref('account.view_move_tree').id, 'tree'), (False, 'form')],
+#            'type': 'ir.actions.act_window',
+#            'domain': [('from_custom_asset', '=', True), ('asset_ref_id', '=', self.id)],
+#            'context': dict(self._context, create=False),
+#        }
 
-    def create_custom_asset_entry(self):
-        for asset in self:
-            asset.compute_custom_depreciation_board()
-            asset.write({'hide_custom_button': True})
+#    def create_custom_asset_entry(self):
+#        for asset in self:
+#            asset.compute_custom_depreciation_board()
+#            asset.write({'hide_custom_button': True})
 
     def compute_custom_depreciation_board(self):
         self.ensure_one()
         depreciation_number = 1
         starting_sequence = 0
         amount_to_depreciate = self.acc_nineteen
-        depreciation_date = self.first_depreciation_date
+        configs = self.env['res.config.settings'].search([])
+        for conf in configs:
+            depreciation_date = conf['assets_date']
         commands = []
         newlines = self._recompute_board(depreciation_number, starting_sequence, amount_to_depreciate,
                                          depreciation_date, already_depreciated_amount=0, amount_change_ids=False)
